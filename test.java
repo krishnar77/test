eb1193f11edfb9afcab0abcbe17f4b6eeb89f141
krishnar77 <krishnar77@gmail.com>
2018-12-06 03:36:00 +0530

    test

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args) {
+//        String s = "Happy";
+//        System.out.println("New Year");
+//        System.out.println("Happy New");
+//        System.out.print("Year ");
+        System.out.print("Happy New");
+        System.out.print("Year");
+//        System.out.println(s);
+//        System.out.print("Happy New!");
+//        System.out.print(" ");
+//        System.out.println("Year");
+    }
+}
